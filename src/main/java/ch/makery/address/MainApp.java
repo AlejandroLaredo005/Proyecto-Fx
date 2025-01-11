251dfe6f9b61eacad5d7648c6ffbf67e98fb0f21
Alejandro Laredo <a_alejandro.laredo.de.los.reyes@iespablopicasso.es>
2025-01-11 19:23:07 +0100

    Cambio cosas

--- src/main/java/ch/makery/address/MainApp.java
+++ src/main/java/ch/makery/address/MainApp.java
@@ -1,17 +1,17 @@
-package main;
+package ch.makery.address;
 
 import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application{
   
   private Stage primaryStage;
-  private BorderPane rootLayout;
+  private SplitPane rootLayout;
 
   @Override
   public void start(Stage primaryStage) {
@@ -26,8 +26,8 @@ public class MainApp extends Application{
       try {
           // Load root layout from fxml file.
           FXMLLoader loader = new FXMLLoader();
-          loader.setLocation(MainApp.class.getResource("../views/InicioSesion.fxml"));
-          rootLayout = (BorderPane) loader.load();
+          loader.setLocation(MainApp.class.getResource("view/InicioSesion.fxml"));
+          rootLayout = (SplitPane) loader.load();
           
           // Show the scene containing the root layout.
           Scene scene = new Scene(rootLayout);
