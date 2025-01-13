05fab12ad4bbe79bb611565397052b70c27fb28d
Alejandro Laredo <a_alejandro.laredo.de.los.reyes@iespablopicasso.es>
2025-01-13 21:09:38 +0100

    Añado nose

--- src/main/java/controllers/InicioSesionController.java
+++ src/main/java/controllers/InicioSesionController.java
@@ -1,7 +1,11 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class InicioSesionController {
   
@@ -10,4 +14,26 @@ public class InicioSesionController {
   
   @FXML
   private TextField txtPassword;
+  
+  @FXML
+  private void registerUser() {
+      try {
+          // Cargar el nuevo archivo FXML
+          FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Registro.fxml"));
+          Parent root = loader.load();
+
+          // Crear una nueva escena y establecerla en el Stage
+          Scene scene = new Scene(root);
+          Stage stage = new Stage();
+          stage.setScene(scene);
+          stage.setTitle("Registro");
+          stage.show();
+
+          // (Opcional) Cerrar la ventana actual si es necesario
+          Stage currentStage = (Stage) txtUsuario.getScene().getWindow();
+          currentStage.close();
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+  }
 }
